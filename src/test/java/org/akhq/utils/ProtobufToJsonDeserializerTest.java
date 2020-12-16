8376a48b19668093d0d78e37b542f78c596363b9
Taisiia Goltseva <tado0618@netcracker.com>
2020-12-16 20:34:30 +0400

    Support ANY field in Protobuf descriptors

--- src/test/java/org/akhq/utils/ProtobufToJsonDeserializerTest.java
+++ src/test/java/org/akhq/utils/ProtobufToJsonDeserializerTest.java
@@ -3,6 +3,7 @@ package org.akhq.utils;
 import com.google.protobuf.Any;
 import com.google.protobuf.DoubleValue;
 import com.google.protobuf.StringValue;
+import com.google.protobuf.Timestamp;
 import org.akhq.configs.Connection.ProtobufDeserializationTopicsMapping;
 import org.akhq.configs.TopicsMapping;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,10 +16,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +24,9 @@ public class ProtobufToJsonDeserializerTest {
     ProtobufDeserializationTopicsMapping protobufDeserializationTopicsMapping;
     AlbumProto.Album albumProto;
     FilmProto.Film filmProto;
-    ComplexProto.Complex complexProtobufObject;
+    ComplexWithBookOutsideProto.ComplexWithBookOutside complexProtobufObjectWithBookOutside;
+    ComplexWithBookInsideProto.ComplexWithBookInside complexProtobufObjectWithBookInside;
+    MultipleLayerComplexProto.MultipleLayerComplex multipleLayerComplex;
 
 
     @BeforeEach
@@ -34,7 +34,9 @@ public class ProtobufToJsonDeserializerTest {
         createTopicProtobufDeserializationMapping();
         createAlbumObject();
         createFilmObject();
-        createComplexObject();
+        createComplexObjectWithBookOutside();
+        createComplexObjectWithBookInside();
+        createMultipleLayerComplex();
     }
 
     private void createTopicProtobufDeserializationMapping() throws URISyntaxException, IOException {
@@ -46,22 +48,34 @@ public class ProtobufToJsonDeserializerTest {
 
         TopicsMapping albumTopicsMapping = new TopicsMapping();
         albumTopicsMapping.setTopicRegex("album.*");
-        albumTopicsMapping.setDescriptorFile("album.desc");
+        String base64AlbumDescriptor = encodeDescriptorFileToBase64("album.desc");
+        albumTopicsMapping.setDescriptorFileBase64(base64AlbumDescriptor);
         albumTopicsMapping.setValueMessageType("Album");
 
         TopicsMapping filmTopicsMapping = new TopicsMapping();
         filmTopicsMapping.setTopicRegex("film.*");
-        String base64FilmDescriptor = encodeDescriptorFileToBase64("film.desc");
-        filmTopicsMapping.setDescriptorFileBase64(base64FilmDescriptor);
+        filmTopicsMapping.setDescriptorFile("film.desc");
         filmTopicsMapping.setValueMessageType("Film");
 
-        TopicsMapping complexObjectTopicsMapping = new TopicsMapping();
-        complexObjectTopicsMapping.setTopicRegex("complex.*");
-        complexObjectTopicsMapping.setDescriptorFile("complex.desc");
-        complexObjectTopicsMapping.setValueMessageType("Complex");
+        TopicsMapping complexObjectTopicsMapping1 = new TopicsMapping();
+        complexObjectTopicsMapping1.setTopicRegex("complex1.*");
+        complexObjectTopicsMapping1.setDescriptorFile("complexWithBookOutside.desc");
+        complexObjectTopicsMapping1.setValueMessageType("ComplexWithBookOutside");
+
+        TopicsMapping complexObjectTopicsMapping2 = new TopicsMapping();
+        complexObjectTopicsMapping2.setTopicRegex("complex2.*");
+        complexObjectTopicsMapping2.setDescriptorFile("complexWithBookInside.desc");
+        complexObjectTopicsMapping2.setValueMessageType("ComplexWithBookInside");
+
+        TopicsMapping multipleLayerComplexObjectTopicsMapping = new TopicsMapping();
+        multipleLayerComplexObjectTopicsMapping.setTopicRegex("multiple.*");
+        multipleLayerComplexObjectTopicsMapping.setDescriptorFile("multipleLayerComplex.desc");
+        multipleLayerComplexObjectTopicsMapping.setValueMessageType("MultipleLayerComplex");
 
         protobufDeserializationTopicsMapping.setTopicsMapping(
-                Arrays.asList(albumTopicsMapping, filmTopicsMapping, complexObjectTopicsMapping));
+                Arrays.asList(albumTopicsMapping, filmTopicsMapping,
+                        complexObjectTopicsMapping1, complexObjectTopicsMapping2,
+                        multipleLayerComplexObjectTopicsMapping));
     }
 
     private String encodeDescriptorFileToBase64(String descriptorFileName) throws URISyntaxException, IOException {
@@ -88,26 +102,56 @@ public class ProtobufToJsonDeserializerTest {
     private void createFilmObject() {
         List<String> starring = Arrays.asList("Harrison Ford", "Mark Hamill", "Carrie Fisher", "Adam Driver", "Daisy Ridley");
         Film film = new Film("Star Wars: The Force Awakens", "J. J. Abrams", 2015, 135, starring);
+        GregorianCalendar date = new GregorianCalendar(2020, Calendar.JANUARY, 16);
+        date.setTimeZone(TimeZone.getTimeZone("GMT"));
         filmProto = FilmProto.Film.newBuilder()
                 .setName(film.getName())
                 .setProducer(film.getProducer())
                 .setReleaseYear(film.getReleaseYear())
                 .setDuration(film.getDuration())
                 .addAllStarring(film.getStarring())
+                .setTimestamp(Any.pack(Timestamp.newBuilder()
+                        .setSeconds(date.getTime().getTime() / 1000)
+                        .build()))
                 .build();
     }
 
-    private void createComplexObject() {
+    private void createComplexObjectWithBookOutside() {
         BookProto.Book bookProto = BookProto.Book.newBuilder()
                 .setTitle("Les Miserables")
                 .setAuthor("Victor Hugo")
                 .setPrice(DoubleValue.newBuilder().setValue(123d))
                 .build();
-        complexProtobufObject = ComplexProto.Complex.newBuilder()
+        complexProtobufObjectWithBookOutside = ComplexWithBookOutsideProto.ComplexWithBookOutside.newBuilder()
                 .setAlbum(albumProto)
                 .setFilm(filmProto)
                 .setAnything(Any.pack(bookProto))
-                .setStringWrapper(StringValue.newBuilder().setValue("stringvalue").build()).build();
+                .setStringWrapper(StringValue.newBuilder()
+                        .setValue("Book message type described in outside proto-file").build())
+                .build();
+    }
+
+    private void createComplexObjectWithBookInside() {
+        BookProto.Book bookProto = BookProto.Book.newBuilder()
+                .setTitle("Les Miserables")
+                .setAuthor("Victor Hugo")
+                .setPrice(DoubleValue.newBuilder().setValue(123d))
+                .build();
+        complexProtobufObjectWithBookInside = ComplexWithBookInsideProto.ComplexWithBookInside.newBuilder()
+                .setAlbum(albumProto)
+                .setFilm(filmProto)
+                .setAnything(Any.pack(bookProto))
+                .setStringWrapper(StringValue.newBuilder()
+                        .setValue("Book message type described inside complex object proto-file").build())
+                .build();
+    }
+
+    private void createMultipleLayerComplex() {
+        multipleLayerComplex = MultipleLayerComplexProto.MultipleLayerComplex.newBuilder()
+                .setAlbum(albumProto)
+                .setFilm(filmProto)
+                .setComplex(complexProtobufObjectWithBookOutside)
+                .build();
     }
 
     @Test
@@ -134,7 +178,11 @@ public class ProtobufToJsonDeserializerTest {
                 "  \"producer\": \"J. J. Abrams\",\n" +
                 "  \"releaseYear\": 2015,\n" +
                 "  \"duration\": 135,\n" +
-                "  \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"]\n" +
+                "  \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"],\n" +
+                "  \"timestamp\": {\n" +
+                "    \"@type\": \"type.googleapis.com/google.protobuf.Timestamp\",\n" +
+                "    \"value\": \"2020-01-16T00:00:00Z\"\n" +
+                "  }\n" +
                 "}";
         assertEquals(expectedFilm, decodedFilm);
     }
@@ -160,15 +208,107 @@ public class ProtobufToJsonDeserializerTest {
     }
 
     @Test
-    public void deserializeComplexObject() {
+    public void deserializeComplexObjectWithBookDescribedInOutsideFile() {
         ProtobufToJsonDeserializer protobufToJsonDeserializer = new ProtobufToJsonDeserializer(protobufDeserializationTopicsMapping);
-        final byte[] binaryComplexObject = complexProtobufObject.toByteArray();
+        final byte[] binaryComplexObject = complexProtobufObjectWithBookOutside.toByteArray();
+        String decodedComplexObject = protobufToJsonDeserializer.deserialize("complex1.topic.name", binaryComplexObject, false);
+        String expectedComplexObject = "{\n" +
+                "  \"album\": {\n" +
+                "    \"title\": \"Origins\",\n" +
+                "    \"artist\": [\"Imagine Dragons\"],\n" +
+                "    \"releaseYear\": 2018,\n" +
+                "    \"songTitle\": [\"Birds\", \"Zero\", \"Natural\", \"Machine\"]\n" +
+                "  },\n" +
+                "  \"film\": {\n" +
+                "    \"name\": \"Star Wars: The Force Awakens\",\n" +
+                "    \"producer\": \"J. J. Abrams\",\n" +
+                "    \"releaseYear\": 2015,\n" +
+                "    \"duration\": 135,\n" +
+                "    \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"]\n" +
+                "  },\n" +
+                "  \"stringWrapper\": \"Book message type described in outside proto-file\",\n" +
+                "  \"anything\": {\n" +
+                "    \"@type\": \"type.googleapis.com/org.akhq.utils.Book\",\n" +
+                "    \"title\": \"Les Miserables\",\n" +
+                "    \"author\": \"Victor Hugo\",\n" +
+                "    \"price\": 123.0\n" +
+                "  }\n" +
+                "}";
+        assertEquals(expectedComplexObject, decodedComplexObject);
+    }
 
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            protobufToJsonDeserializer.deserialize("complex.topic.name", binaryComplexObject, false);
-        });
-        String expectedMessage = "Cannot deserialize message with Protobuf deserializer";
-        String actualMessage = exception.getMessage();
-        assertTrue(actualMessage.contains(expectedMessage));
+    @Test
+    public void deserializeComplexObjectWithBookDescribedInside() {
+        ProtobufToJsonDeserializer protobufToJsonDeserializer = new ProtobufToJsonDeserializer(protobufDeserializationTopicsMapping);
+        final byte[] binaryComplexObject = complexProtobufObjectWithBookInside.toByteArray();
+        String decodedComplexObject = protobufToJsonDeserializer.deserialize("complex2.topic.name", binaryComplexObject, false);
+        String expectedComplexObject = "{\n" +
+                "  \"album\": {\n" +
+                "    \"title\": \"Origins\",\n" +
+                "    \"artist\": [\"Imagine Dragons\"],\n" +
+                "    \"releaseYear\": 2018,\n" +
+                "    \"songTitle\": [\"Birds\", \"Zero\", \"Natural\", \"Machine\"]\n" +
+                "  },\n" +
+                "  \"film\": {\n" +
+                "    \"name\": \"Star Wars: The Force Awakens\",\n" +
+                "    \"producer\": \"J. J. Abrams\",\n" +
+                "    \"releaseYear\": 2015,\n" +
+                "    \"duration\": 135,\n" +
+                "    \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"]\n" +
+                "  },\n" +
+                "  \"stringWrapper\": \"Book message type described inside complex object proto-file\",\n" +
+                "  \"anything\": {\n" +
+                "    \"@type\": \"type.googleapis.com/org.akhq.utils.Book\",\n" +
+                "    \"title\": \"Les Miserables\",\n" +
+                "    \"author\": \"Victor Hugo\",\n" +
+                "    \"price\": 123.0\n" +
+                "  }\n" +
+                "}";
+        assertEquals(expectedComplexObject, decodedComplexObject);
+    }
+
+    @Test
+    public void deserializeMultipleLayerComplex() {
+        ProtobufToJsonDeserializer protobufToJsonDeserializer = new ProtobufToJsonDeserializer(protobufDeserializationTopicsMapping);
+        final byte[] binaryMultipleLayerComplexObject = multipleLayerComplex.toByteArray();
+        String decodedMultipleLayerComplexObject = protobufToJsonDeserializer.deserialize("multiple.complex.topic.name", binaryMultipleLayerComplexObject, false);
+        String expectedMultipleLayerComplexObject = "{\n" +
+                "  \"album\": {\n" +
+                "    \"title\": \"Origins\",\n" +
+                "    \"artist\": [\"Imagine Dragons\"],\n" +
+                "    \"releaseYear\": 2018,\n" +
+                "    \"songTitle\": [\"Birds\", \"Zero\", \"Natural\", \"Machine\"]\n" +
+                "  },\n" +
+                "  \"film\": {\n" +
+                "    \"name\": \"Star Wars: The Force Awakens\",\n" +
+                "    \"producer\": \"J. J. Abrams\",\n" +
+                "    \"releaseYear\": 2015,\n" +
+                "    \"duration\": 135,\n" +
+                "    \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"]\n" +
+                "  },\n" +
+                "  \"complex\": {\n" +
+                "    \"album\": {\n" +
+                "      \"title\": \"Origins\",\n" +
+                "      \"artist\": [\"Imagine Dragons\"],\n" +
+                "      \"releaseYear\": 2018,\n" +
+                "      \"songTitle\": [\"Birds\", \"Zero\", \"Natural\", \"Machine\"]\n" +
+                "    },\n" +
+                "    \"film\": {\n" +
+                "      \"name\": \"Star Wars: The Force Awakens\",\n" +
+                "      \"producer\": \"J. J. Abrams\",\n" +
+                "      \"releaseYear\": 2015,\n" +
+                "      \"duration\": 135,\n" +
+                "      \"starring\": [\"Harrison Ford\", \"Mark Hamill\", \"Carrie Fisher\", \"Adam Driver\", \"Daisy Ridley\"]\n" +
+                "    },\n" +
+                "    \"stringWrapper\": \"Book message type described in outside proto-file\",\n" +
+                "    \"anything\": {\n" +
+                "      \"@type\": \"type.googleapis.com/org.akhq.utils.Book\",\n" +
+                "      \"title\": \"Les Miserables\",\n" +
+                "      \"author\": \"Victor Hugo\",\n" +
+                "      \"price\": 123.0\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        assertEquals(expectedMultipleLayerComplexObject, decodedMultipleLayerComplexObject);
     }
 }
